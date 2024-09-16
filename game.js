c4317b17da1d9838b51a23cc68818c87b3b570a9
sandworm <necroa14@gmail.com>
2024-09-16 18:28:07 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,6 @@
-
+window.addeventlistener('load', function(){
+const canvas = document.getelementbyid('canvas1')
+  const ctx = canvas.getcontext('2d')
+  canvas.width = 800;
+  canvas.height = 700;
+});
